980cb500d6b0e1b5575518bdea4deb814cbae7a9
zhailiang <zhailiang@haoduocheyou.com>
2017-08-16 11:03:33 +0800

    3-8

--- imooc-security-demo/src/test/java/com/imooc/web/controller/UserControllerTest.java
+++ imooc-security-demo/src/test/java/com/imooc/web/controller/UserControllerTest.java
@@ -4,6 +4,7 @@
 package com.imooc.web.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -20,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -42,6 +44,16 @@ public class UserControllerTest {
 	public void setup() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 	}
+	
+	@Test
+	public void whenUploadSuccess() throws Exception {
+		String result = mockMvc.perform(fileUpload("/file")
+				.file(new MockMultipartFile("file", "test.txt", "multipart/form-data", "hello upload".getBytes("UTF-8"))))
+				.andExpect(status().isOk())
+				.andReturn().getResponse().getContentAsString();
+		System.out.println(result);
+	}
+	
 
 	@Test
 	public void whenQuerySuccess() throws Exception {
