5fb659ce49ed174e3bf96b711558c63b4277896c
zhailiang <zhailiang@haoduocheyou.com>
2017-08-20 03:23:19 +0800

    4-02

---
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.imooc.security.browser;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zhailiang
+ *
+ */
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.formLogin()
+//		http.httpBasic()
+			.and()
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated();
+		
+	}
+
+}
