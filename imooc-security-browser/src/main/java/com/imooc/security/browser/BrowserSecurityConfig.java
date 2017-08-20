a60c0eb98c082fdf36e3c66642b6d294f805f95c
zhailiang <zhailiang@haoduocheyou.com>
2017-08-21 01:31:04 +0800

    4-04

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -3,6 +3,7 @@
  */
 package com.imooc.security.browser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,12 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.imooc.security.core.properties.SecurityProperties;
+
 /**
  * @author zhailiang
  *
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private SecurityProperties securityProperties;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -27,11 +33,17 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.formLogin()
+			.loginPage("/authentication/require")
+			.loginProcessingUrl("/authentication/form")
 //		http.httpBasic()
 			.and()
 			.authorizeRequests()
+			.antMatchers("/authentication/require",
+					securityProperties.getBrowser().getLoginPage()).permitAll()
 			.anyRequest()
-			.authenticated();
+			.authenticated()
+			.and()
+			.csrf().disable();
 		
 	}
 
---
+++ imooc-security-core/src/main/java/com/imooc/security/core/properties/BrowserProperties.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.imooc.security.core.properties;
+
+/**
+ * @author zhailiang
+ *
+ */
+public class BrowserProperties {
+	
+	private String loginPage = "/imooc-signIn.html";
+
+	public String getLoginPage() {
+		return loginPage;
+	}
+
+	public void setLoginPage(String loginPage) {
+		this.loginPage = loginPage;
+	}
+	
+}
