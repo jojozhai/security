515867ad08b45968d4897b0a8fd49dc7d3ac360b
zhailiang <zhailiang@haoduocheyou.com>
2017-08-23 01:14:51 +0800

    4-7

--- imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ValidateCodeController.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ValidateCodeController.java
@@ -3,13 +3,17 @@
  */
 package com.imooc.security.core.validate.code;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.web.HttpSessionSessionStrategy;
 import org.springframework.social.connect.web.SessionStrategy;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,16 +31,68 @@ public class ValidateCodeController {
 	
 	private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
 	
-	@Autowired
-	private ValidateCodeGenerator validateCodeGenerator;
-	
 	@GetMapping("/code/image")
 	public void createCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
-		ImageCode imageCode = validateCodeGenerator.generate(new ServletWebRequest(request));
+		ImageCode imageCode = generate(new ServletWebRequest(request));
 		sessionStrategy.setAttribute(new ServletWebRequest(request), SESSION_KEY, imageCode);
 		ImageIO.write(imageCode.getImage(), "JPEG", response.getOutputStream());
 		
+	}
+	
+	private ImageCode generate(ServletWebRequest request) {
+		int width = 67;
+		int height = 23;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+		Graphics g = image.getGraphics();
+
+		Random random = new Random();
+
+		g.setColor(getRandColor(200, 250));
+		g.fillRect(0, 0, width, height);
+		g.setFont(new Font("Times New Roman", Font.ITALIC, 20));
+		g.setColor(getRandColor(160, 200));
+		for (int i = 0; i < 155; i++) {
+			int x = random.nextInt(width);
+			int y = random.nextInt(height);
+			int xl = random.nextInt(12);
+			int yl = random.nextInt(12);
+			g.drawLine(x, y, x + xl, y + yl);
+		}
+
+		String sRand = "";
+		for (int i = 0; i < 4; i++) {
+			String rand = String.valueOf(random.nextInt(10));
+			sRand += rand;
+			g.setColor(new Color(20 + random.nextInt(110), 20 + random.nextInt(110), 20 + random.nextInt(110)));
+			g.drawString(rand, 13 * i + 6, 16);
+		}
+
+		g.dispose();
+
+		return new ImageCode(image, sRand, 60);
+	}
+	
+	/**
+	 * 生成随机背景条纹
+	 * 
+	 * @param fc
+	 * @param bc
+	 * @return
+	 */
+	private Color getRandColor(int fc, int bc) {
+		Random random = new Random();
+		if (fc > 255) {
+			fc = 255;
+		}
+		if (bc > 255) {
+			bc = 255;
+		}
+		int r = fc + random.nextInt(bc - fc);
+		int g = fc + random.nextInt(bc - fc);
+		int b = fc + random.nextInt(bc - fc);
+		return new Color(r, g, b);
 	}
 
 }
