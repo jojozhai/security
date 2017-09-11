0b0d6734bbc804546a1c78bbebc232e2afb4be91
zhailiang <zhailiang@haoduocheyou.com>
2017-09-12 00:52:52 +0800

    update

--- imooc-security-app/src/main/java/com/imooc/security/app/SpringSocialConfigurerPostProcessor.java
+++ imooc-security-app/src/main/java/com/imooc/security/app/SpringSocialConfigurerPostProcessor.java
@@ -17,23 +17,29 @@ import com.imooc.security.core.social.ImoocSpringSocialConfigurer;
 @Component
 public class SpringSocialConfigurerPostProcessor implements BeanPostProcessor {
 
-	/* (non-Javadoc)
-	 * @see org.springframework.beans.factory.config.BeanPostProcessor#postProcessBeforeInitialization(java.lang.Object, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.beans.factory.config.BeanPostProcessor#
+	 * postProcessBeforeInitialization(java.lang.Object, java.lang.String)
 	 */
 	@Override
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 		return bean;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.springframework.beans.factory.config.BeanPostProcessor#postProcessAfterInitialization(java.lang.Object, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.beans.factory.config.BeanPostProcessor#
+	 * postProcessAfterInitialization(java.lang.Object, java.lang.String)
 	 */
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-		if(StringUtils.equals(beanName, "imoocSocialSecurityConfig")){
-			ImoocSpringSocialConfigurer config = (ImoocSpringSocialConfigurer)bean;
-			config.signupUrl("/social/signUp");
-			return config;
+		if (StringUtils.equals(beanName, "imoocSocialSecurityConfig")) {
+			ImoocSpringSocialConfigurer configurer = (ImoocSpringSocialConfigurer)bean;
+			configurer.signupUrl("/social/signUp");
+			return configurer;
 		}
 		return bean;
 	}
