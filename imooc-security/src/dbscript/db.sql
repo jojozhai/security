6577ec56a04e604b3b0d45321355c3149d7cc6e0
zhailiang <zhailiang@haoduocheyou.com>
2017-09-13 08:50:47 +0800

    add db script

---
+++ imooc-security/src/dbscript/db.sql
@@ -0,0 +1,19 @@
+-- 记住我功能用的表
+create table persistent_logins (username varchar(64) not null,
+								series varchar(64) primary key,
+								token varchar(64) not null,
+								last_used timestamp not null);
+-- 社交登录用的表
+create table UserConnection (userId varchar(255) not null,
+	providerId varchar(255) not null,
+	providerUserId varchar(255),
+	rank int not null,
+	displayName varchar(255),
+	profileUrl varchar(512),
+	imageUrl varchar(512),
+	accessToken varchar(512) not null,
+	secret varchar(512),
+	refreshToken varchar(512),
+	expireTime bigint,
+	primary key (userId, providerId, providerUserId));
+create unique index UserConnectionRank on UserConnection(userId, providerId, rank);
