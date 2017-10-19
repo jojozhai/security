9eebd07d2ce7ddbc702f228db1c4fccf7d4605ff
zhailiang <zhailiang@haoduocheyou.com>
2017-10-19 16:57:09 +0800

    debug

--- imooc-security-core/src/main/java/com/imooc/security/core/social/view/ImoocConnectView.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/social/view/ImoocConnectView.java
@@ -30,7 +30,7 @@ public class ImoocConnectView extends AbstractView {
 			HttpServletResponse response) throws Exception {
 
 		response.setContentType("text/html;charset=UTF-8");
-		if (model.get("connection") == null) {
+		if (model.get("connections") == null) {
 			response.getWriter().write("<h3>解绑成功</h3>");
 		} else {
 			response.getWriter().write("<h3>绑定成功</h3>");
